cc01969d019fca666d5d35074312f11b3aea7a3c
Mathieu Fosse <mathieu@tigerlilyapps.com>
2015-11-26 11:08:30 +0100

    Use Docker version 1.9.1

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # This Script aims to setup a docker environment for OSX (Maybe Linux) with docker client, docker-machine and docker-compose
 # All binaries are installed in /usr/local/bin/ directory.
 
-DOCKER_VERSION="1.9.1-rc1"
+DOCKER_VERSION="1.9.1"
 DOCKER_MACHINE_VERSION="0.5.1"
 #DOCKER_COMPOSE_VERSION="1.5.0rc1"
 
