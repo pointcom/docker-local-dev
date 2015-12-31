933487b9bda53a4b2ddf15aba21158c10b16a61f
Mathieu Fosse <mathieu@tigerlilyapps.com>
2015-12-31 14:14:58 +0100

    Use docker machine 0.5.5

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 # All binaries are installed in /usr/local/bin/ directory.
 
 DOCKER_VERSION="1.9.1"
-DOCKER_MACHINE_VERSION="0.5.4"
+DOCKER_MACHINE_VERSION="0.5.5"
 #DOCKER_COMPOSE_VERSION="1.5.0rc1"
 
 RED='\033[0;31m'
