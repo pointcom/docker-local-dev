89396bd5bac412b53caef09e282a72b7d49eb3fb
Mathieu Fosse <mathieu@tigerlilyapps.com>
2016-02-08 19:47:43 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 # This Script aims to setup a docker environment for OSX (Maybe Linux) with docker client, docker-machine and docker-compose
 # All binaries are installed in /usr/local/bin/ directory.
 
-DOCKER_VERSION="1.10.0-rc2"
-DOCKER_MACHINE_VERSION="0.6.0-rc1"
-DOCKER_COMPOSE_VERSION="1.6.0-rc1"
+DOCKER_VERSION="1.10.0"
+DOCKER_MACHINE_VERSION="0.6.0"
+DOCKER_COMPOSE_VERSION="1.6.0"
 
 RED='\033[0;31m'
 GREEN='\033[0;32m'
